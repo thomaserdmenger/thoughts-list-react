557d71b2be42caad8ed80bc1a99c6bba6cd66e78
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-11-06 06:50:11 +0100

    Hard code thoughts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
-export default function App() {}
+import * as React from "react";
+
+const initialThoughts = [
+  { id: 1, text: "This is a place for your passing thoughts." },
+  { id: 2, text: "They'll be removed after 15 seconds." },
+];
+
+export default function App() {
+  const [thoughts, setThoughts] = React.useState(initialThoughts);
+
+  return (
+    <React.Fragment>
+      <header>
+        <h1>💭 Passing Thoughts</h1>
+      </header>
+      <main>
+        <ul>
+          {thoughts.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </main>
+    </React.Fragment>
+  );
+}
